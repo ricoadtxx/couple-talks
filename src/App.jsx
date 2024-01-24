de97e30ba50e6b97aceef7cd7887250d908a49c1
ricoadtxx <euricoadit555@gmail.com>
2024-01-25 01:02:09 +0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "/pages/Home";
-import Deeptalk from "/pages/Deeptalk";
+import Home from "./pages/Home";
+import Deeptalk from "./pages/Deeptalk";
 
 export default function App() {
 	return (
